3c1c4799242e44e98777fe4332d3a2a21111b342
Ian Rae <ian.rae@snhu.edu>
2023-05-11 10:46:20 -0400

    added basic routes for build and userPart

--- server.mjs
+++ server.mjs
@@ -2,6 +2,8 @@ import express from "express";
 import cors from "cors";
 import "./loadEnvironment.mjs"
 import records from "./routes/record.mjs";
+import builds from "./routes/build.mjs";
+import userParts from "./routes/userPart.mjs";
 
 const PORT = process.env.PORT || 5050;
 const app = express();
@@ -10,6 +12,8 @@ app.use(cors());
 app.use(express.json());
 
 app.use("/record", records);
+app.use("/builds", builds)
+app.use("/userParts", userParts)
 
 app.listen(PORT, () => {
     console.log(`Server is running on port: ${PORT}`);
